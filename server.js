e2aa7e1b3827ba4e7d695fee99b039e89ca9e44d
Muhammad Abu Sufyan <Chsufyan3087278@gmail.com>
2023-02-02 12:43:07 +0500

    Graph QL schema
    Graph QL schema setup in app

--- server.js
+++ server.js
@@ -1,6 +1,25 @@
 const express = require('express');
+const { graphqlHTTP } = require('express-graphql');
+const expressGraphQL = require('express-graphql').graphqlHTTP
+const {GraphQLSchema, GraphQLObjectType, GraphQLString} = require('graphql');
 const app = express();
 const port = 5000
 
+const schema = new GraphQLSchema({
+  query: new GraphQLObjectType({
+    name: 'query',
+    fields: () => ({
+      message: {
+        type: GraphQLString,
+        resolve: () => 'Hello World',
+      },
+    })
+  })
+})
+
+app.use('/graphql', expressGraphQL({
+  graphiql: true,
+  schema: schema
+}))
 
 app.listen(port, () => console.log(`listening on port ${port}`));
